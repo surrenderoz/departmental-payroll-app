0f1967d2cf902f00d1132ed3a3a3d68e3ee27ff2
surrenderoz <surendraverma.verma5@gmail.com>
2022-11-13 14:00:35 +0530

    added firebase support

---
+++ src/utils/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
